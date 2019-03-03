eb3651ff8d6e71c63e2c57ac34858b2ed6828551
Kim Meyer <kim.meyer@palasthotel.de>
2019-03-03 13:13:16 +0100

    gutenberg support update

--- disable-attachment-pages.php
+++ disable-attachment-pages.php
@@ -2,18 +2,18 @@
 /**
  * Plugin Name: Disable Attachment Pages
  * Description: Redirects attachment pages to the post, where they are placed, and hides backend option to link images to attachment page (if not default).
- * Version: 1.0
+ * Version: 1.1
  * Author: PALASTHOTEL by Kim-Christian Meyer
  * Author URI: https://palasthotel.de
  */
 
 
+add_action( 'template_redirect', 'disable_attachment_pages_redirect_attachments', 1 );
 function disable_attachment_pages_redirect_attachments() {
-	global $post;
 	if ( is_attachment() ) {
-		if ( ! empty( $post->post_parent ) ) {
+		if ( ! empty( $GLOBALS['post']->post_parent ) ) {
 			// Permanent redirect to post/page, where the image or document was placed.
-			wp_redirect( get_permalink( $post->post_parent ), 301 );
+			wp_redirect( get_permalink( $GLOBALS['post']->post_parent ), 301 );
 			exit;
 		}
 
@@ -22,21 +22,21 @@ function disable_attachment_pages_redirect_attachments() {
 		exit;
 	}
 }
-add_action( 'template_redirect', 'disable_attachment_pages_redirect_attachments', 1 );
 
 
 /**
  * Hide the 'attachment page' option for the link-to part when editing or
  * inserting images.
  */
+add_action( 'admin_head', 'disable_attachment_pages_disable_linkto' );
 function disable_attachment_pages_disable_linkto() {
 	echo <<<EOT
 <style>
 .setting select.link-to option[value="post"],
-.setting select[data-setting="link"] option[value="post"] {
+.setting select[data-setting="link"] option[value="post"],
+.components-select-control__input option[value="attachment"] {
 	display: none;
 }
 </style>
 EOT;
 }
-add_action( 'admin_head', 'disable_attachment_pages_disable_linkto' );
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: palasthotel, greatestview
 Donate link: https://palasthotel.de/
 Tags: redirect, attachments, attachment, images
 Requires at least: 4.0
-Tested up to: 4.7.3
-Stable tag: 1.0
+Tested up to: 5.1
+Stable tag: 1.1
 License: GNU General Public License v3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -21,6 +21,9 @@ Further, when editing a post, the option to link images to their attachment page
 3. You’re done! Try following an attachment link, the browser should redirect back to the post, where this link is placed.
 
 == Changelog ==
+
+= 1.1 =
+* Added Gutenberg support.
 
 = 1.0 =
 * First release
